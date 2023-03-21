b26ed4fa563126d82f264738371c2d57be4a27f8
akima <akima@AKI>
2023-03-21 21:38:37 +0200

    Add project files.

---
+++ SQL_paskaa/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SQLServerDatabaseGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tableComboBox = new System.Windows.Forms.ComboBox();
+            this.labelTable = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 47);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(760, 502);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(697, 15);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // tableComboBox
+            // 
+            this.tableComboBox.Location = new System.Drawing.Point(90, 15);
+            this.tableComboBox.Name = "tableComboBox";
+            this.tableComboBox.Size = new System.Drawing.Size(200, 21);
+            this.tableComboBox.TabIndex = 2;
+            this.tableComboBox.SelectedIndexChanged += new System.EventHandler(this.tableComboBox_SelectedIndexChanged);
+            // 
+            // labelTable
+            // 
+            this.labelTable.AutoSize = true;
+            this.labelTable.Location = new System.Drawing.Point(12, 18);
+            this.labelTable.Name = "labelTable";
+            this.labelTable.Size = new System.Drawing.Size(72, 13);
+            this.labelTable.TabIndex = 3;
+            this.labelTable.Text = "Select Table:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.labelTable);
+            this.Controls.Add(this.tableComboBox);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "SQL Server Database GUI";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox tableComboBox;
+        private System.Windows.Forms.Label labelTable;
+    }
+}
---
+++ SQL_paskaa/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SQLServerDatabaseGUI
+{
+    public partial class Form1 : Form
+    {
+        private string ConnectionString = "Data Source=SERVER;Initial Catalog=DATABASE;Integrated Security=True";
+        private SqlDataAdapter dataAdapter;
+        private DataTable dataTable;
+        private SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTableNames();
+        }
+
+        private void LoadTableNames()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                DataTable schema = connection.GetSchema("Tables");
+                tableComboBox.DisplayMember = "TABLE_NAME";
+                tableComboBox.DataSource = schema;
+            }
+        }
+
+        private void LoadData()
+        {
+            string tableName = tableComboBox.Text;
+
+            connection = new SqlConnection(ConnectionString);
+            dataAdapter = new SqlDataAdapter("SELECT * FROM " + tableName, connection);
+
+            SqlCommandBuilder builder = new SqlCommandBuilder(dataAdapter);
+            dataAdapter.UpdateCommand = builder.GetUpdateCommand();
+
+            dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            dataGridView.DefaultCellStyle.Padding = new Padding(0);
+            dataGridView.DataSource = dataTable;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView.EndEdit();
+
+                // Trim whitespace from the values
+                foreach (DataGridViewRow row in dataGridView.Rows)
+                {
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        if (cell.Value != null)
+                        {
+                            cell.Value = cell.Value.ToString().Trim();
+                        }
+                    }
+                }
+
+                connection.Open();
+                dataAdapter.Update(dataTable);
+                connection.Close();
+
+                MessageBox.Show("Data saved successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+        private void tableComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
